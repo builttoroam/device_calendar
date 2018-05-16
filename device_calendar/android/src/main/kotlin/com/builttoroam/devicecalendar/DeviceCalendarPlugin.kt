5385d7d6d17cde8380b893d3426f58c963fd6301
mkieres <mikolaj.kieres@progrunning.net>
2018-05-16 17:14:04 +1000

    Creating a plugin

---
+++ device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/DeviceCalendarPlugin.kt
@@ -0,0 +1,25 @@
+package com.builttoroam.devicecalendar
+
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+class DeviceCalendarPlugin(): MethodCallHandler {
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar): Unit {
+      val channel = MethodChannel(registrar.messenger(), "device_calendar")
+      channel.setMethodCallHandler(DeviceCalendarPlugin())
+    }
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result): Unit {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+}
