1dfc4c5a49b585fa45852b157c51deb171ea40d1
mkieres <mikolaj.kieres@progrunning.net>
2018-05-17 16:06:23 +1000

    Retrieving device calendars and displaying them on screen

--- device_calendar/example/lib/main.dart
+++ device_calendar/example/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:device_calendar/device_calendar.dart';
+import 'package:device_calendar/models/calendar.dart';
+import 'package:device_calendar/services/calendars.dart';
 
 void main() => runApp(new MyApp());
 
@@ -11,6 +13,8 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  List<Calendar> _calendars = new List<Calendar>();
+  CalendarsService _calendarsService = new CalendarsService();
 
   @override
   initState() {
@@ -21,9 +25,11 @@ class _MyAppState extends State<MyApp> {
   // Platform messages are asynchronous, so we initialize in an async method.
   initPlatformState() async {
     String platformVersion;
+    List<Calendar> calendars;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       platformVersion = await DeviceCalendar.platformVersion;
+      calendars = await _calendarsService.retrieveCalendars();
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -31,11 +37,11 @@ class _MyAppState extends State<MyApp> {
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
-    if (!mounted)
-      return;
+    if (!mounted) return;
 
     setState(() {
       _platformVersion = platformVersion;
+      _calendars = calendars;
     });
   }
 
@@ -43,13 +49,25 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Plugin example app'),
-        ),
-        body: new Center(
-          child: new Text('Running on: $_platformVersion\n'),
-        ),
-      ),
+          appBar: new AppBar(
+            title: new Text('Plugin example app'),
+          ),
+          body: new Column(
+            children: <Widget>[
+              new Center(
+                child: new Text('Running on: $_platformVersion\n'),
+              ),
+              new Expanded(
+                flex: 1,
+                child: new ListView.builder(
+                  itemCount: _calendars.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return new Text(_calendars[index].name);
+                  },
+                ),
+              )
+            ],
+          )),
     );
   }
 }
---
+++ device_calendar/lib/models/calendar.dart
@@ -0,0 +1,10 @@
+/// Device calendar abstraction
+class Calendar {
+  /// Platform-specific unique calendar identifier
+  String id;
+
+  /// Calendar display name
+  String name;
+
+  Calendar(this.id, {this.name});
+}
