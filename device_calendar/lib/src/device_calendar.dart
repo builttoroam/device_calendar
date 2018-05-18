802867313682f91fbe180a562a9b57027ab9b70b
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2018-05-18 16:51:36 +1000

    Format code

--- device_calendar/lib/src/device_calendar.dart
+++ device_calendar/lib/src/device_calendar.dart
@@ -1,7 +1,9 @@
 part of device_calendar;
+
 /// A singleton service providing functionality to work with device calendar(s)
 class DeviceCalendarPlugin {
-  static const MethodChannel channel = const MethodChannel('plugins.builttoroam.com/device_calendar');
+  static const MethodChannel channel =
+      const MethodChannel('plugins.builttoroam.com/device_calendar');
 
   static final DeviceCalendarPlugin _instance =
       new DeviceCalendarPlugin._createInstance();
@@ -15,28 +17,20 @@ class DeviceCalendarPlugin {
   /// Retrieves all of the device defined calendars
   Future<List<Calendar>> retrieveCalendars() async {
     try {
-      var calendarsJson =
-          await channel.invokeMethod('retrieveCalendars');
-      return _parseCalendars(calendarsJson);
+      var calendarsJson = await channel.invokeMethod('retrieveCalendars');
+      final List<Calendar> calendars = new List<Calendar>();
+
+      var decodedCalendars = json.decode(calendarsJson);
+      for (var decodedCalendar in decodedCalendars) {
+        var calendar = new Calendar.fromJson(decodedCalendar);
+        calendars.add(calendar);
+      }
+
+      return calendars;
     } catch (e) {
       print(e);
     }
 
     return new List<Calendar>();
   }
-}
-
-List<Calendar> _parseCalendars(String calendarsJson) {
-    final List<Calendar> calendars = new List<Calendar>();
-
-  var decodedCalendars = json.decode(calendarsJson);
-  for(var decodedCalendar in decodedCalendars) {
-    var calendar = new Calendar.fromJson(decodedCalendar);
-    calendars.add(calendar);
-  }
-  // for (var item in calendarsJson) {
-  //   calendars.add(new Calendar("1", name: item));
-  // }
-
-  return calendars;
 }
