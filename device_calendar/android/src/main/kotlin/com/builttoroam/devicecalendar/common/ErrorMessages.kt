1b91a40ab00e1018da874a4dd832d31002e7f7b3
mkieres <mikolaj.kieres@progrunning.net>
2018-05-21 15:01:09 +1000

    Moving literal code to constants

---
+++ device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/common/ErrorMessages.kt
@@ -0,0 +1,7 @@
+package com.builttoroam.devicecalendar.common
+
+class ErrorMessages {
+    companion object {
+        const val CALENDAR_ID_INVALID_ARGUMENT_MESSAGE: String = "Calendar ID is not a number";
+    }
+}
