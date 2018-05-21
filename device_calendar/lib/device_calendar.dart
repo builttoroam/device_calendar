61449b72d5110359e61201183f3f5b04460253a7
mkieres <mikolaj.kieres@progrunning.net>
2018-05-21 15:25:30 +1000

    Throwing an exception when mapped json is null

--- device_calendar/lib/device_calendar.dart
+++ device_calendar/lib/device_calendar.dart
@@ -4,6 +4,8 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/services.dart';
 
+import 'src/common/constants.dart';
+
 part 'src/models/calendar.dart';
 part 'src/models/event.dart';
 part 'src/device_calendar.dart';
---
+++ device_calendar/lib/src/common/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const String fromJsonMapIsNull = 'The json object is null';
+}
