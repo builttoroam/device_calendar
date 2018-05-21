6e7649277304b09e512983fc6bc8bdbf09256284
mkieres <mikolaj.kieres@progrunning.net>
2018-05-21 15:01:09 +1000

    Moving literal code to constants

---
+++ device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/common/ErrorCodes.kt
@@ -0,0 +1,9 @@
+package com.builttoroam.devicecalendar.common
+
+class ErrorCodes {
+    companion object {
+        const val EXCEPTION: String = "exception";
+        const val INVALID_ARGUMENT: String = "invalid_argument";
+        const val CALENDAR_RETRIEVAL_FAILURE: String = "calendar_retrieval_failure";
+    }
+}
