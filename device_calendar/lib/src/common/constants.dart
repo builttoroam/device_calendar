573a0ac96c723f60442f8341515a71a46773303b
mkieres <mikolaj.kieres@progrunning.net>
2018-05-22 16:51:07 +1000

    Adding create event functionality

--- device_calendar/lib/src/common/constants.dart
+++ device_calendar/lib/src/common/constants.dart
@@ -1,3 +1,6 @@
 class Constants {
   static const String fromJsonMapIsNull = 'The json object is null';
+
+  static const String invalidArgument = 'Provided arguments are invalid';
+  static const String createEventArgumentReuirements = 'To create an event you must provide calendar with an id, event with title, start date and end date';
 }
