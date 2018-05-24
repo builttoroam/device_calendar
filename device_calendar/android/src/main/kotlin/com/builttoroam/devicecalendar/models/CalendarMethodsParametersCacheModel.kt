237395bd57739dd99a82d4dd3a756c530e5b9e4e
mkieres <mikolaj.kieres@progrunning.net>
2018-05-24 10:42:09 +1000

    Refactoring code to handle concurrent calls

--- device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/models/CalendarMethodsParametersCacheModel.kt
+++ device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/models/CalendarMethodsParametersCacheModel.kt
@@ -1,9 +1,13 @@
 package com.builttoroam.devicecalendar.models
 
-class CalendarMethodsParametersCacheModel(val calendarServiceMethodCode: Int,
+import io.flutter.plugin.common.MethodChannel
+
+class CalendarMethodsParametersCacheModel(val pendingChannelResult: MethodChannel.Result,
+                                          val calendarServiceMethodCode: Int,
                                           var calendarId: String = "",
                                           var calendarEventsStartDate: Long = -1,
                                           var calendarEventsEndDate: Long = -1,
                                           var eventId: String = "",
                                           var event: Event? = null) {
+    var ownCacheKey: Int? = null;
 }
