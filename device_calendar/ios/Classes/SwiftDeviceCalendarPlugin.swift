723ad4c02e3c7f90ad8f60d3c4a33d1902b0e168
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2018-05-24 14:26:09 +1000

    Add ability to delete events and fixing bug when adding an event. Also added placeholder error codes and messages

--- device_calendar/ios/Classes/SwiftDeviceCalendarPlugin.swift
+++ device_calendar/ios/Classes/SwiftDeviceCalendarPlugin.swift
@@ -17,11 +17,18 @@ public class SwiftDeviceCalendarPlugin: NSObject, FlutterPlugin {
     }
     
     static let channelName = "plugins.builttoroam.com/device_calendar"
-    let errorCode = "error"
+    // Note: this is placeholder error codes and messages until they have been finalised across both platforms
+    let notFoundErrorCode = "404";
+    let genericError = "500"
+    let unauthorizedErrorCode = "401"
+    let unauthorizedErrorMessage = "The user has not allowed this application to modify their calendar"
+    let calendarNotFoundErrorMessage = "The calendar with the specified id could not be found"
+    let eventNotFoundErrorMessage = "The event with the specified id could not be found"
     let eventStore = EKEventStore()
     let retrieveCalendarsMethod = "retrieveCalendars"
     let retrieveEventsMethod = "retrieveEvents"
     let createOrUpdateEventMethod = "createOrUpdateEvent"
+    let deleteEventMethod = "deleteEvent"
     let calendarIdArgument = "calendarId"
     let startDateArgument = "startDate"
     let endDateArgument = "endDate"
@@ -41,7 +48,7 @@ public class SwiftDeviceCalendarPlugin: NSObject, FlutterPlugin {
         switch call.method {
         case retrieveCalendarsMethod:
             requestPermission(completion: {
-                (accessGranted: Bool) in
+                (accessGranted) in
                 if(accessGranted) {
                     let ekCalendars = self.eventStore.calendars(for: .event)
                     var calendars = [Calendar]()
@@ -52,12 +59,12 @@ public class SwiftDeviceCalendarPlugin: NSObject, FlutterPlugin {
                     
                     self.encodeJsonAndFinish(codable: calendars, result: result)
                 } else {
-                    result(nil)
+                    self.finishWithUnauthorizedError(result: result)
                 }
             })
         case retrieveEventsMethod:
             requestPermission(completion: {
-                (accessGranted: Bool) in
+                (accessGranted) in
                 if(accessGranted) {
                     let arguments = call.arguments as! Dictionary<String, AnyObject>
                     let calendarId = arguments[self.calendarIdArgument] as! String
@@ -76,17 +83,17 @@ public class SwiftDeviceCalendarPlugin: NSObject, FlutterPlugin {
                     
                     self.encodeJsonAndFinish(codable: events, result: result)
                 } else {
-                    result(nil)
+                    self.finishWithUnauthorizedError(result: result)
                 }
                 
             })
         case createOrUpdateEventMethod:
             requestPermission(completion: {
-                (accessGranted: Bool) in
-                if(accessGranted) {
+                (accessGranted) in
+                if (accessGranted) {
                     let arguments = call.arguments as! Dictionary<String, AnyObject>
                     let calendarId = arguments[self.calendarIdArgument] as! String
-                    let eventId = arguments[self.eventIdArgument] as! String
+                    let eventId = arguments[self.eventIdArgument] as? String
                     let startDateMillisecondsSinceEpoch = arguments[self.eventStartDateArgument] as! NSNumber
                     let endDateDateMillisecondsSinceEpoch = arguments[self.eventEndDateArgument] as! NSNumber
                     let startDate = Date (timeIntervalSince1970: startDateMillisecondsSinceEpoch.doubleValue / 1000.0)
@@ -94,9 +101,20 @@ public class SwiftDeviceCalendarPlugin: NSObject, FlutterPlugin {
                     let title = arguments[self.eventTitleArgument] as! String
                     let description = arguments[self.eventDescriptionArgument] as! String
                     let ekCalendar = self.eventStore.calendar(withIdentifier: calendarId)
-                    var ekEvent = self.eventStore.event(withIdentifier: eventId)
-                    if(ekEvent == nil) {
+                    if (ekCalendar == nil) {
+                        self.finishWithCalendarNotFoundError(result: result)
+                        return
+                    }
+                    
+                    var ekEvent: EKEvent?
+                    if(eventId == nil) {
                         ekEvent = EKEvent.init(eventStore: self.eventStore)
+                    } else {
+                        ekEvent = self.eventStore.event(withIdentifier: eventId!)
+                        if(ekEvent == nil) {
+                            self.finishWithEventNotFoundError(result: result)
+                            return
+                        }
                     }
                     ekEvent!.title = title
                     ekEvent!.notes = description
@@ -107,15 +125,57 @@ public class SwiftDeviceCalendarPlugin: NSObject, FlutterPlugin {
                         try self.eventStore.save(ekEvent!, span: EKSpan.futureEvents)
                         result(ekEvent!.eventIdentifier)
                     } catch {
-                        result(FlutterError(code: self.errorCode, message: error.localizedDescription, details: nil))
+                        self.eventStore.reset()
+                        result(FlutterError(code: self.genericError, message: error.localizedDescription, details: nil))
                     }
                 } else {
-                    result(nil)
+                    self.finishWithUnauthorizedError(result: result)
+                }
+            })
+        case deleteEventMethod:
+            requestPermission(completion: {
+                (accessGranted) in
+                if(accessGranted) {
+                    let arguments = call.arguments as! Dictionary<String, AnyObject>
+                    let calendarId = arguments[self.calendarIdArgument] as! String
+                    let eventId = arguments[self.eventIdArgument] as! String
+                    let ekCalendar = self.eventStore.calendar(withIdentifier: calendarId)
+                    if (ekCalendar == nil) {
+                        self.finishWithCalendarNotFoundError(result: result)
+                        return;
+                    }
+                    let ekEvent = self.eventStore.event(withIdentifier: eventId)
+                    if (ekEvent == nil) {
+                        self.finishWithEventNotFoundError(result: result)
+                        return
+                    }
+                    
+                    do {
+                        try self.eventStore.remove(ekEvent!, span: .thisEvent)
+                        result(true)
+                    } catch {
+                        self.eventStore.reset()
+                        result(FlutterError(code: self.genericError, message: error.localizedDescription, details: nil))
+                    }
+                } else {
+                    self.finishWithUnauthorizedError(result: result)
                 }
             })
         default:
             result(FlutterMethodNotImplemented)
         }
+    }
+    
+    private func finishWithUnauthorizedError(result: @escaping FlutterResult) {
+        result(FlutterError(code:self.unauthorizedErrorCode, message: self.unauthorizedErrorMessage, details: nil))
+    }
+    
+    private func finishWithCalendarNotFoundError(result: @escaping FlutterResult) {
+        result(FlutterError(code:self.notFoundErrorCode, message: self.calendarNotFoundErrorMessage, details: nil))
+    }
+    
+    private func finishWithEventNotFoundError(result: @escaping FlutterResult) {
+        result(FlutterError(code:self.notFoundErrorCode, message: self.eventNotFoundErrorMessage, details: nil))
     }
     
     private func encodeJsonAndFinish<T: Codable>(codable: T, result: @escaping FlutterResult) {
@@ -125,7 +185,7 @@ public class SwiftDeviceCalendarPlugin: NSObject, FlutterPlugin {
             let jsonString = String(data: jsonData, encoding: .utf8)
             result(jsonString)
         } catch {
-            result(FlutterError(code: errorCode, message: error.localizedDescription, details: nil))
+            result(FlutterError(code: genericError, message: error.localizedDescription, details: nil))
         }
     }
     
