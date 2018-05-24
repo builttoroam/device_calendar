9c0f56c4d9ca2d9a93b52ab13ae43ab7fb441512
mkieres <mikolaj.kieres@progrunning.net>
2018-05-24 11:27:25 +1000

    Removing semicolons from kotlin files

--- device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/common/Constants.kt
+++ device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/common/Constants.kt
@@ -4,11 +4,11 @@ import android.provider.CalendarContract
 
 class Constants {
     companion object {
-        const val CALENDAR_PROJECTION_ID_INDEX: Int = 0;
-        const val CALENDAR_PROJECTION_ACCOUNT_NAME_INDEX: Int = 1;
-        const val CALENDAR_PROJECTION_DISPLAY_NAME_INDEX: Int = 2;
-        const val CALENDAR_PROJECTION_OWNER_ACCOUNT_INDEX: Int = 3;
-        const val CALENDAR_PROJECTION_ACCESS_LEVEL_INDEX: Int = 4;
+        const val CALENDAR_PROJECTION_ID_INDEX: Int = 0
+        const val CALENDAR_PROJECTION_ACCOUNT_NAME_INDEX: Int = 1
+        const val CALENDAR_PROJECTION_DISPLAY_NAME_INDEX: Int = 2
+        const val CALENDAR_PROJECTION_OWNER_ACCOUNT_INDEX: Int = 3
+        const val CALENDAR_PROJECTION_ACCESS_LEVEL_INDEX: Int = 4
 
         val CALENDAR_PROJECTION: Array<String> = arrayOf(
                 CalendarContract.Calendars._ID,                           // 0
@@ -16,15 +16,15 @@ class Constants {
                 CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
                 CalendarContract.Calendars.OWNER_ACCOUNT,                 // 3
                 CalendarContract.Calendars.CALENDAR_ACCESS_LEVEL          // 4
-        );
+        )
 
-        const val EVENT_PROJECTION_ID_INDEX: Int = 0;
-        const val EVENT_PROJECTION_TITLE_INDEX: Int = 1;
-        const val EVENT_PROJECTION_DESCRIPTION_INDEX: Int = 2;
-        const val EVENT_PROJECTION_START_DATE_INDEX: Int = 3;
-        const val EVENT_PROJECTION_END_DATE_INDEX: Int = 4;
-        const val EVENT_PROJECTION_ALL_DAY_INDEX: Int = 5;
-        const val EVENT_PROJECTION_EVENT_LOCATION_INDEX: Int = 6;
+        const val EVENT_PROJECTION_ID_INDEX: Int = 0
+        const val EVENT_PROJECTION_TITLE_INDEX: Int = 1
+        const val EVENT_PROJECTION_DESCRIPTION_INDEX: Int = 2
+        const val EVENT_PROJECTION_START_DATE_INDEX: Int = 3
+        const val EVENT_PROJECTION_END_DATE_INDEX: Int = 4
+        const val EVENT_PROJECTION_ALL_DAY_INDEX: Int = 5
+        const val EVENT_PROJECTION_EVENT_LOCATION_INDEX: Int = 6
 
         val EVENT_PROJECTION: Array<String> = arrayOf(
                 CalendarContract.Events._ID,
@@ -34,6 +34,6 @@ class Constants {
                 CalendarContract.Instances.DTEND,
                 CalendarContract.Events.ALL_DAY,
                 CalendarContract.Events.EVENT_LOCATION
-        );
+        )
     }
 }
--- device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/models/Event.kt
+++ device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/models/Event.kt
@@ -1,8 +1,8 @@
 package com.builttoroam.devicecalendar.models
 
 class Event(val title: String) {
-    var id: String? = null;
-    var description: String? = null;
-    var start: Long = -1;
-    var end: Long = -1;
+    var id: String? = null
+    var description: String? = null
+    var start: Long = -1
+    var end: Long = -1
 }
