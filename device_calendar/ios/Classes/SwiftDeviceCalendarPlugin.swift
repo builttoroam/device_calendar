b2bb39d7198d8a5cdb84eb6960c2a47fb2be8d32
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2018-05-30 12:14:06 +1000

    refactor code out into separate methods when handle the various methods from Flutter

--- device_calendar/ios/Classes/SwiftDeviceCalendarPlugin.swift
+++ device_calendar/ios/Classes/SwiftDeviceCalendarPlugin.swift
@@ -47,112 +47,130 @@ public class SwiftDeviceCalendarPlugin: NSObject, FlutterPlugin {
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
     
-    
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
         case requestPermissionsMethod:
             requestPermissions(result)
         case hasPermissionsMethod:
-            let hasPermissions = self.hasPermissions()
-            result(hasPermissions)
+            hasPermissions(result)
         case retrieveCalendarsMethod:
-            checkPermissionsThenExecute(permissionsGrantedAction: {
-                let ekCalendars = self.eventStore.calendars(for: .event)
-                var calendars = [Calendar]()
-                for ekCalendar in ekCalendars {
-                    let calendar = Calendar(id: ekCalendar.calendarIdentifier, name: ekCalendar.title)
-                    calendars.append(calendar)
-                }
-                
-                self.encodeJsonAndFinish(codable: calendars, result: result)
-            }, result: result)
-            
+            retrieveCalendars(result)
         case retrieveEventsMethod:
-            checkPermissionsThenExecute(permissionsGrantedAction: {
-                let arguments = call.arguments as! Dictionary<String, AnyObject>
-                let calendarId = arguments[self.calendarIdArgument] as! String
-                let startDateMillisecondsSinceEpoch = arguments[self.startDateArgument] as! NSNumber
-                let endDateDateMillisecondsSinceEpoch = arguments[self.endDateArgument] as! NSNumber
-                let startDate = Date (timeIntervalSince1970: startDateMillisecondsSinceEpoch.doubleValue / 1000.0)
-                let endDate = Date (timeIntervalSince1970: endDateDateMillisecondsSinceEpoch.doubleValue / 1000.0)
-                let ekCalendar = self.eventStore.calendar(withIdentifier: calendarId)
-                let predicate = self.eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: [ekCalendar!])
-                let ekEvents = self.eventStore.events(matching: predicate)
-                var events = [Event]()
-                for ekEvent in ekEvents {
-                    let event = Event(eventId: ekEvent.eventIdentifier, calendarId: calendarId, title: ekEvent.title, description: ekEvent.notes, start: Int(ekEvent.startDate.timeIntervalSince1970) * 1000, end: Int(ekEvent.endDate.timeIntervalSince1970) * 1000)
-                    events.append(event)
-                }
-                
-                self.encodeJsonAndFinish(codable: events, result: result)
-            }, result: result)
+            retrieveEvents(call, result)
         case createOrUpdateEventMethod:
-            checkPermissionsThenExecute(permissionsGrantedAction: {
-                let arguments = call.arguments as! Dictionary<String, AnyObject>
-                let calendarId = arguments[self.calendarIdArgument] as! String
-                let eventId = arguments[self.eventIdArgument] as? String
-                let startDateMillisecondsSinceEpoch = arguments[self.eventStartDateArgument] as! NSNumber
-                let endDateDateMillisecondsSinceEpoch = arguments[self.eventEndDateArgument] as! NSNumber
-                let startDate = Date (timeIntervalSince1970: startDateMillisecondsSinceEpoch.doubleValue / 1000.0)
-                let endDate = Date (timeIntervalSince1970: endDateDateMillisecondsSinceEpoch.doubleValue / 1000.0)
-                let title = arguments[self.eventTitleArgument] as! String
-                let description = arguments[self.eventDescriptionArgument] as! String
-                let ekCalendar = self.eventStore.calendar(withIdentifier: calendarId)
-                if (ekCalendar == nil) {
-                    self.finishWithCalendarNotFoundError(result: result)
-                    return
-                }
-                
-                var ekEvent: EKEvent?
-                if(eventId == nil) {
-                    ekEvent = EKEvent.init(eventStore: self.eventStore)
-                } else {
-                    ekEvent = self.eventStore.event(withIdentifier: eventId!)
-                    if(ekEvent == nil) {
-                        self.finishWithEventNotFoundError(result: result)
-                        return
-                    }
-                }
-                ekEvent!.title = title
-                ekEvent!.notes = description
-                ekEvent!.startDate = startDate
-                ekEvent!.endDate = endDate
-                ekEvent!.calendar = ekCalendar
-                do {
-                    try self.eventStore.save(ekEvent!, span: EKSpan.futureEvents)
-                    result(ekEvent!.eventIdentifier)
-                } catch {
-                    self.eventStore.reset()
-                    result(FlutterError(code: self.genericError, message: error.localizedDescription, details: nil))
-                }
-            }, result: result)
+            createOrUpdateEvent(call, result)
         case deleteEventMethod:
-            checkPermissionsThenExecute(permissionsGrantedAction: {
-                let arguments = call.arguments as! Dictionary<String, AnyObject>
-                let calendarId = arguments[self.calendarIdArgument] as! String
-                let eventId = arguments[self.eventIdArgument] as! String
-                let ekCalendar = self.eventStore.calendar(withIdentifier: calendarId)
-                if (ekCalendar == nil) {
-                    self.finishWithCalendarNotFoundError(result: result)
-                    return;
-                }
-                let ekEvent = self.eventStore.event(withIdentifier: eventId)
-                if (ekEvent == nil) {
-                    self.finishWithEventNotFoundError(result: result)
-                    return
-                }
-                
-                do {
-                    try self.eventStore.remove(ekEvent!, span: .thisEvent)
-                    result(true)
-                } catch {
-                    self.eventStore.reset()
-                    result(FlutterError(code: self.genericError, message: error.localizedDescription, details: nil))
-                }
-            }, result: result)
+            deleteEvent(call, result)
         default:
             result(FlutterMethodNotImplemented)
         }
+    }
+    
+    private func hasPermissions(_ result: FlutterResult) {
+        let hasPermissions = self.hasPermissions()
+        result(hasPermissions)
+    }
+    
+    private func retrieveCalendars(_ result: @escaping FlutterResult) {
+        checkPermissionsThenExecute(permissionsGrantedAction: {
+            let ekCalendars = self.eventStore.calendars(for: .event)
+            var calendars = [Calendar]()
+            for ekCalendar in ekCalendars {
+                let calendar = Calendar(id: ekCalendar.calendarIdentifier, name: ekCalendar.title)
+                calendars.append(calendar)
+            }
+            
+            self.encodeJsonAndFinish(codable: calendars, result: result)
+        }, result: result)
+    }
+    
+    private func retrieveEvents(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
+        checkPermissionsThenExecute(permissionsGrantedAction: {
+            let arguments = call.arguments as! Dictionary<String, AnyObject>
+            let calendarId = arguments[self.calendarIdArgument] as! String
+            let startDateMillisecondsSinceEpoch = arguments[self.startDateArgument] as! NSNumber
+            let endDateDateMillisecondsSinceEpoch = arguments[self.endDateArgument] as! NSNumber
+            let startDate = Date (timeIntervalSince1970: startDateMillisecondsSinceEpoch.doubleValue / 1000.0)
+            let endDate = Date (timeIntervalSince1970: endDateDateMillisecondsSinceEpoch.doubleValue / 1000.0)
+            let ekCalendar = self.eventStore.calendar(withIdentifier: calendarId)
+            let predicate = self.eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: [ekCalendar!])
+            let ekEvents = self.eventStore.events(matching: predicate)
+            var events = [Event]()
+            for ekEvent in ekEvents {
+                let event = Event(eventId: ekEvent.eventIdentifier, calendarId: calendarId, title: ekEvent.title, description: ekEvent.notes, start: Int(ekEvent.startDate.timeIntervalSince1970) * 1000, end: Int(ekEvent.endDate.timeIntervalSince1970) * 1000)
+                events.append(event)
+            }
+            
+            self.encodeJsonAndFinish(codable: events, result: result)
+        }, result: result)
+    }
+    
+    private func createOrUpdateEvent(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
+        checkPermissionsThenExecute(permissionsGrantedAction: {
+            let arguments = call.arguments as! Dictionary<String, AnyObject>
+            let calendarId = arguments[self.calendarIdArgument] as! String
+            let eventId = arguments[self.eventIdArgument] as? String
+            let startDateMillisecondsSinceEpoch = arguments[self.eventStartDateArgument] as! NSNumber
+            let endDateDateMillisecondsSinceEpoch = arguments[self.eventEndDateArgument] as! NSNumber
+            let startDate = Date (timeIntervalSince1970: startDateMillisecondsSinceEpoch.doubleValue / 1000.0)
+            let endDate = Date (timeIntervalSince1970: endDateDateMillisecondsSinceEpoch.doubleValue / 1000.0)
+            let title = arguments[self.eventTitleArgument] as! String
+            let description = arguments[self.eventDescriptionArgument] as! String
+            let ekCalendar = self.eventStore.calendar(withIdentifier: calendarId)
+            if (ekCalendar == nil) {
+                self.finishWithCalendarNotFoundError(result: result)
+                return
+            }
+            
+            var ekEvent: EKEvent?
+            if(eventId == nil) {
+                ekEvent = EKEvent.init(eventStore: self.eventStore)
+            } else {
+                ekEvent = self.eventStore.event(withIdentifier: eventId!)
+                if(ekEvent == nil) {
+                    self.finishWithEventNotFoundError(result: result)
+                    return
+                }
+            }
+            ekEvent!.title = title
+            ekEvent!.notes = description
+            ekEvent!.startDate = startDate
+            ekEvent!.endDate = endDate
+            ekEvent!.calendar = ekCalendar
+            do {
+                try self.eventStore.save(ekEvent!, span: EKSpan.futureEvents)
+                result(ekEvent!.eventIdentifier)
+            } catch {
+                self.eventStore.reset()
+                result(FlutterError(code: self.genericError, message: error.localizedDescription, details: nil))
+            }
+        }, result: result)
+    }
+    
+    private func deleteEvent(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
+        checkPermissionsThenExecute(permissionsGrantedAction: {
+            let arguments = call.arguments as! Dictionary<String, AnyObject>
+            let calendarId = arguments[self.calendarIdArgument] as! String
+            let eventId = arguments[self.eventIdArgument] as! String
+            let ekCalendar = self.eventStore.calendar(withIdentifier: calendarId)
+            if (ekCalendar == nil) {
+                self.finishWithCalendarNotFoundError(result: result)
+                return;
+            }
+            let ekEvent = self.eventStore.event(withIdentifier: eventId)
+            if (ekEvent == nil) {
+                self.finishWithEventNotFoundError(result: result)
+                return
+            }
+            
+            do {
+                try self.eventStore.remove(ekEvent!, span: .thisEvent)
+                result(true)
+            } catch {
+                self.eventStore.reset()
+                result(FlutterError(code: self.genericError, message: error.localizedDescription, details: nil))
+            }
+        }, result: result)
     }
     
     private func finishWithUnauthorizedError(result: @escaping FlutterResult) {
