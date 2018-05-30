13e6ef16db9377fe3d8f7f3110ab8876e74155ce
mkieres <mikolaj@builttoroam.com>
2018-05-30 17:19:11 +1000

    Returning event attendees

--- device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/models/Attendee.kt
+++ device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/models/Attendee.kt
@@ -2,6 +2,7 @@ package com.builttoroam.devicecalendar.models
 
 class Attendee(val name: String) {
     var id: Long = -1
+    var eventId: Long = -1
     var email: String? = null
     var attendanceRequired: Boolean = false
 }
