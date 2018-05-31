e165f18eb9dd9d1c9006c91617654baea312f351
mkieres <mikolaj@builttoroam.com>
2018-05-31 10:58:52 +1000

    Replacing exception error code with the unified version

--- device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/common/ErrorCodes.kt
+++ device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/common/ErrorCodes.kt
@@ -2,7 +2,6 @@ package com.builttoroam.devicecalendar.common
 
 class ErrorCodes {
     companion object {
-        const val EXCEPTION: String = "exception"
         const val INVALID_ARGUMENT: String = "400"
         const val NOT_FOUND: String = "404"
         const val NOT_ALLOWED: String = "405"
