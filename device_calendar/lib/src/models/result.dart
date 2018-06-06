e072962f80058f2263c78d46c421db5d4a97b2c6
mkieres <mikolaj@builttoroam.com>
2018-06-06 11:22:10 +1000

    Error handling code cleanup

--- device_calendar/lib/src/models/result.dart
+++ device_calendar/lib/src/models/result.dart
@@ -1,15 +1,17 @@
 part of device_calendar;
 
 class Result<T> {
-  bool isSuccess = false;
+  bool get isSuccess {
+    var res = data != null && errorMessages.length == 0;
+    if (res) {
+      if (data is String) {
+        res = (data as String).isNotEmpty;
+      }
+    }
+
+    return res;
+  }
+
   T data;
   List<String> errorMessages = new List<String>();
-
-    // TODO: Change to allow constructor parameter
-  Result([this.data]);
-
-   // TODO: Add wrapper to set data (and isSuccess to true)
-
-   // TODO: To add error message (and set isSuccess to false);
-
 }
