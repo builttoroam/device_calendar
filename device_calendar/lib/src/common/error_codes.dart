24edd3994cbb081dba77b04d107033ff635c2f97
mkieres <mikolaj@builttoroam.com>
2018-06-06 11:22:10 +1000

    Error handling code cleanup

--- device_calendar/lib/src/common/error_codes.dart
+++ device_calendar/lib/src/common/error_codes.dart
@@ -1,4 +1,6 @@
 class ErrorCodes {
   static const int invalidArguments = 400;
-  // TODO: Add error code 500 for when we get an error back from the device level
+  static const int platformSpecific = 599;
+  static const int generic = 500;
+  static const int unknown = 502;
 }
