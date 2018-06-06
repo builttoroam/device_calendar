8392291e3ddde23f8a5e4c4b3ce7f57bca46f8f4
mkieres <mikolaj@builttoroam.com>
2018-06-06 12:00:45 +1000

    Updating comments on the calendar API

--- device_calendar/lib/src/models/result.dart
+++ device_calendar/lib/src/models/result.dart
@@ -1,6 +1,9 @@
 part of device_calendar;
 
 class Result<T> {
+  /// Indicates if the request was successfull or not
+  ///
+  /// Returns true if data is not null and there're no error messages, otherwise returns false
   bool get isSuccess {
     var res = data != null && errorMessages.length == 0;
     if (res) {
