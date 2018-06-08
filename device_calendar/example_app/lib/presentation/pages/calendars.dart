c95bafecba97cc8913c0f689e4475bfd35975539
mkieres <mikolaj@builttoroam.com>
2018-06-08 15:33:06 +1000

    Adding sample code

--- device_calendar/example_app/lib/presentation/pages/calendars.dart
+++ device_calendar/example_app/lib/presentation/pages/calendars.dart
@@ -28,44 +28,45 @@ class _CalendarsPageState extends State<CalendarsPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Calendars'),
-        ),
-        body: new Column(
-          children: <Widget>[
-            new Expanded(
-              flex: 1,
-              child: new ListView.builder(
-                itemCount: _calendars?.length ?? 0,
-                itemBuilder: (BuildContext context, int index) {
-                  return new GestureDetector(
-                      onTap: () async {
-                        await Navigator.push(context, new MaterialPageRoute(
-                            builder: (BuildContext context) {
-                          return new CalendarEventsPage(_calendars[index]);
-                        }));
-                      },
-                      child: new Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: new Row(
-                            children: <Widget>[
-                              new Expanded(
-                                flex: 1,
-                                child: new Text(
-                                  _calendars[index].name,
-                                  style: new TextStyle(fontSize: 25.0),
-                                ),
+      appBar: new AppBar(
+        title: new Text('Calendars'),
+      ),
+      body: new Column(
+        children: <Widget>[
+          new Expanded(
+            flex: 1,
+            child: new ListView.builder(
+              itemCount: _calendars?.length ?? 0,
+              itemBuilder: (BuildContext context, int index) {
+                return new GestureDetector(
+                    onTap: () async {
+                      await Navigator.push(context, new MaterialPageRoute(
+                          builder: (BuildContext context) {
+                        return new CalendarEventsPage(_calendars[index]);
+                      }));
+                    },
+                    child: new Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: new Row(
+                          children: <Widget>[
+                            new Expanded(
+                              flex: 1,
+                              child: new Text(
+                                _calendars[index].name,
+                                style: new TextStyle(fontSize: 25.0),
                               ),
-                              new Icon(_calendars[index].isReadOnly
-                                  ? Icons.lock
-                                  : Icons.lock_open)
-                            ],
-                          )));
-                },
-              ),
-            )
-          ],
-        ));
+                            ),
+                            new Icon(_calendars[index].isReadOnly
+                                ? Icons.lock
+                                : Icons.lock_open)
+                          ],
+                        )));
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   void _retrieveCalendars() async {
