9350be4d369abd0dfb20e63b068f5e9d25dbeedc
alexandru.cozmanca <alexandru.cozmanca@gmail.com>
2019-05-09 18:06:38 +0300

    Updated attendee model

--- device_calendar/lib/src/models/attendee.dart
+++ device_calendar/lib/src/models/attendee.dart
@@ -4,20 +4,34 @@ part of device_calendar;
 class Attendee {
   /// The name of the attendee
   String name;
+  String email;
+  bool attendanceRequired;
+  int eventId;
+  int id;
 
-  Attendee(this.name);
+  Attendee(this.name, {this.email, this.attendanceRequired, this.eventId, this.id});
 
   Attendee.fromJson(Map<String, dynamic> json) {
+
     if (json == null) {
       throw new ArgumentError(ErrorMessages.fromJsonMapIsNull);
     }
 
     name = json['name'];
+    email = json['email'];
+    attendanceRequired = json['attendanceRequired'] ?? false;
+    eventId = json['eventId'];
+    id = json['id'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['name'] = this.name;
+    data['email'] = this.email;
+    data['attendanceRequired'] = this.attendanceRequired;
+    data['eventId'] = this.eventId;
+    data['id'] = this.id;
+
     return data;
   }
 }
