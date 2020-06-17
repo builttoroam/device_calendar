43a3670125e4520f448658555d79059e3df219c1
Nick Randolph <nick@builttoroam.com>
2020-06-17 14:36:31 +1000

    Refactoring dart channel method calls

--- lib/src/models/result.dart
+++ lib/src/models/result.dart
@@ -13,6 +13,14 @@ class Result<T> {
     return res;
   }
 
+  /// Indicates if there are errors. This isn't exactly the same as !isSuccess since
+  /// it doesn't look at the state of the data.
+  ///
+  /// Returns true if there are error messages, otherwise false
+  bool get hasErrors {
+    return errorMessages.isNotEmpty;
+  }
+
   T data;
   List<String> errorMessages = <String>[];
 }
