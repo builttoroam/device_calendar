eee6a2b3708a4bd99ec574b33776288915267643
Nick Randolph <nick@builttoroam.com>
2020-06-19 00:28:39 +1000

    Removing remaining string literals

--- lib/src/common/channel_constants.dart
+++ lib/src/common/channel_constants.dart
@@ -9,4 +9,16 @@ class ChannelConstants {
   static const String methodNameDeleteEventInstance = 'deleteEventInstance';
   static const String methodNameCreateOrUpdateEvent = 'createOrUpdateEvent';
   static const String methodNameCreateCalendar = 'createCalendar';
+
+  static const String parameterNameCalendarId = 'calendarId';
+  static const String parameterNameStartDate = 'startDate';
+  static const String parameterNameEndDate = 'endDate';
+  static const String parameterNameEventId = 'eventId';
+  static const String parameterNameEventIds = 'eventIds';
+  static const String parameterNameEventStartDate = 'eventStartDate';
+  static const String parameterNameEventEndDate = 'eventEndDate';
+  static const String parameterNameFollowingInstances = 'followingInstances';
+  static const String parameterNameCalendarName = 'calendarName';
+  static const String parameterNameCalendarColor = 'calendarColor';
+  static const String parameterNameLocalAccountName = 'localAccountName';
 }
