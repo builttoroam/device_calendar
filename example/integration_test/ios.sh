69e1b175bd7bc9ddd1db4ad09c2c1f0e2c1b12b3
sowens-csd <stephen.owens@thoughtwire.com>
2021-12-24 14:24:17 -0500

    documentation for the shell script

--- example/integration_test/ios.sh
+++ example/integration_test/ios.sh
@@ -1,8 +1,17 @@
+# Use: integration_test/ios.sh 
+#
+# Executes the device_calendar integration test for iOS
+# This script creates and starts a new iOS simulator, grants calendar permission 
+# to the app then runs the integration tests and finally deletes the simulator.
+# Prerequisites: Xcode, Xcode Command Line Tools, Xcode iOS Simulator
+#
+# Success - "All tests passed." is printed to the console
+#
 deviceId=$(xcrun simctl create builtToRoamCalendarTest "iPhone 13" 2> /dev/null | tail -1)
 echo "Created device: $deviceId, booting..."
 xcrun simctl boot $deviceId
 xcrun simctl privacy $deviceId grant calendar com.builttoroam.deviceCalendarExample00
 echo "Running tests..."
 flutter drive --driver=integration_test/integration_test.dart --target=integration_test/app_test.dart -d $deviceId
-echo "Removing up device: $deviceId"
+echo "Removing device: $deviceId"
 xcrun simctl delete $deviceId
