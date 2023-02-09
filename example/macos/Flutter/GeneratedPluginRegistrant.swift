c2cfb2f1a119623c715dcba27c414080dfe52dda
Julius Bredemeyer <48645716+IVLIVS-III@users.noreply.github.com>
2023-02-09 03:44:05 +0100

    Added macos to the example app.

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_native_timezone
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
+}
